e74821422237d84e474d40c6d7345d2a9ae1629e
ana-balica <ana.balica@gmail.com>
2014-11-27 21:41:48 +0800

    store position of a SquareButton
    * create row and column custom attributes
    * create position SquareButton attribute

--- app/src/main/java/anabalica/github/io/meowletters/customviews/SquareButton.java
+++ app/src/main/java/anabalica/github/io/meowletters/customviews/SquareButton.java
@@ -1,8 +1,14 @@
 package anabalica.github.io.meowletters.customviews;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.ArrayMap;
 import android.util.AttributeSet;
 import android.widget.Button;
+
+import java.util.HashMap;
+
+import anabalica.github.io.meowletters.R;
 
 /**
  * SquareButton width and height are set according to the CustomTableRow width
@@ -12,9 +18,24 @@ import android.widget.Button;
  * @author Ana Balica
  */
 public class SquareButton extends Button {
+    private Integer row;
+    private Integer column;
+    private HashMap<String, Integer> position = new HashMap<String, Integer>(2);
 
     public SquareButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray attributes = context.getTheme().obtainStyledAttributes(
+                attrs, R.styleable.SquareButton, 0, 0);
+
+        try {
+            row = attributes.getInteger(R.styleable.SquareButton_row, 0);
+            column = attributes.getInteger(R.styleable.SquareButton_column, 0);
+        } finally {
+            attributes.recycle();
+        }
+        position.put("row", row);
+        position.put("column", column);
     }
 
     @Override
@@ -27,5 +48,17 @@ public class SquareButton extends Button {
         final int width = parentWidth / columnsCount;
 
         setMeasuredDimension(width, width);
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public HashMap<String, Integer> getPosition() {
+        return position;
     }
 }
