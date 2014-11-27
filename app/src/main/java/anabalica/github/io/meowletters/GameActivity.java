717a3c13fafa54cfc919d147ec261fbe02377946
ana-balica <ana.balica@gmail.com>
2014-11-27 21:42:24 +0800

    create a dummy grid for the game activity

--- app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
+++ app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 /**
@@ -41,5 +42,9 @@ public class GameActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void selectLetter(View view) {
+        System.out.println("Button pressed");
     }
 }
