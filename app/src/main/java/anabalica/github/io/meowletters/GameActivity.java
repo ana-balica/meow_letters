c6a7ef61243847270c2b0c9b581dcda0c8e005e0
ana-balica <ana.balica@gmail.com>
2014-12-03 19:22:26 +0800

    draw first letters on grid on create

--- app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
+++ app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
@@ -6,7 +6,12 @@ import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+
+import anabalica.github.io.meowletters.letters.Letter;
+import anabalica.github.io.meowletters.letters.LetterChain;
+import anabalica.github.io.meowletters.letters.LetterGrid;
 
 
 /**
@@ -23,6 +28,8 @@ public class GameActivity extends Activity {
     private int millisInterval = 5;
     private int millisReset = 0;
 
+    private LetterGrid letterGrid;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +42,12 @@ public class GameActivity extends Activity {
         // Start the countdown timer
         timer = new GameCountDownTimer(millisTotal, millisInterval);
         timer.start();
+
+        // Initialize the letter grid and initial letter chain
+        letterGrid = new LetterGrid(5, 5);
+        LetterChain letterChain = LetterChain.generateChain(2, 1);
+        letterGrid.addLetterChain(letterChain);
+        drawLetterButtons();
     }
 
     @Override
@@ -61,6 +74,51 @@ public class GameActivity extends Activity {
 
     public void selectLetter(View view) {
         System.out.println("Button pressed");
+    }
+
+    /**
+     * Translate the LetterGrid to the actual UI grid by drawing the buttons,
+     * their letter and their correct position.
+     */
+    public void drawLetterButtons() {
+        int rows = letterGrid.getROWS();
+        int columns = letterGrid.getCOLUMNS();
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                if (letterGrid.getGrid()[row][column] != null) {
+                    drawLetterButton(letterGrid.getGrid()[row][column], row, column);
+                }
+            }
+        }
+    }
+
+    /**
+     * Draw a LetterButton on the UI grid in the provided position
+     *
+     * @param letter Letter object
+     * @param row int the row position
+     * @param column int the column position
+     */
+    public void drawLetterButton(Letter letter, int row, int column) {
+        String letterButtonId = buildLetterButtonId(row, column);
+
+        int id = this.getResources().getIdentifier(letterButtonId, "id", this.getPackageName());
+        Button button = (Button) findViewById(id);
+        button.setVisibility(View.VISIBLE);
+        button.setText(letter.getLetter());
+    }
+
+
+    /**
+     * Pick the position of a letter button and create the string ID of this
+     * button
+     *
+     * @param row int the row position
+     * @param column int the column position
+     * @return String button ID
+     */
+    private String buildLetterButtonId(int row, int column) {
+        return "letter_button_" + row + column;
     }
 
     /**
