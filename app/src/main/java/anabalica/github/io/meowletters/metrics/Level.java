f7ae4a4c870cd7f8b761ee098a05d01de286fd30
ana-balica <ana.balica@gmail.com>
2014-12-10 22:37:34 +0800

    add dummy classes for Score and Level

---
+++ app/src/main/java/anabalica/github/io/meowletters/metrics/Level.java
@@ -0,0 +1,10 @@
+package anabalica.github.io.meowletters.metrics;
+
+/**
+ * Level class keeps current user level. User starts from level 1 and advances
+ * as much as possible. There is no top limit.
+ *
+ * @author Ana Balica
+ */
+public class Level {
+}
---
+++ app/src/main/java/anabalica/github/io/meowletters/metrics/Score.java
@@ -0,0 +1,10 @@
+package anabalica.github.io.meowletters.metrics;
+
+/**
+ * Score class keeps the current user score. User starts from score 0 and keeps
+ * gaining more for each correct move. There is no top limit.
+ *
+ * @author Ana Balica
+ */
+public class Score {
+}
