354707e411fd554a8d0d498d4f0864ed7203c68b
ana-balica <ana.balica@gmail.com>
2014-12-11 18:52:16 +0800

    update Cell class
    * add setters for row and column
    * generate hashCode and equals

--- app/src/main/java/anabalica/github/io/meowletters/letters/Cell.java
+++ app/src/main/java/anabalica/github/io/meowletters/letters/Cell.java
@@ -10,8 +10,8 @@ public class Cell {
     private int column;
 
     public Cell(int row, int column) {
-        this.row = row;
-        this.column = column;
+        setRow(row);
+        setColumn(column);
     }
 
     public int getRow() {
@@ -20,5 +20,39 @@ public class Cell {
 
     public int getColumn() {
         return column;
+    }
+
+    public void setRow(int row) throws IllegalArgumentException {
+        if (row < 0) {
+            throw new IllegalArgumentException("Row can not be negative.");
+        }
+        this.row = row;
+    }
+
+    public void setColumn(int column) throws IllegalArgumentException {
+        if (column < 0) {
+            throw new IllegalArgumentException("Column can not be negative.");
+        }
+        this.column = column;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        if (column != cell.column) return false;
+        if (row != cell.row) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = row;
+        result = 31 * result + column;
+        return result;
     }
 }
