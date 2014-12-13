399dca09a6cac88aa9c47564fad6b4cdb0999b74
ana-balica <ana.balica@gmail.com>
2014-12-13 19:46:45 +0800

    implement Level class

--- app/src/main/java/anabalica/github/io/meowletters/metrics/Level.java
+++ app/src/main/java/anabalica/github/io/meowletters/metrics/Level.java
@@ -7,4 +7,36 @@ package anabalica.github.io.meowletters.metrics;
  * @author Ana Balica
  */
 public class Level {
+    private int level;
+
+    public Level() {
+        level = 1;
+    }
+
+    public Level(int level) {
+        setLevel(level);
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) throws IllegalArgumentException {
+        if (level < 1) {
+            throw new IllegalArgumentException("Level must be a positive number");
+        }
+        this.level = level;
+    }
+
+    public int updateLevel(int points) {
+        if (points > 0) {
+            level = (Integer) (points / 50) + 1;
+        }
+        return level;
+    }
+
+    public Level reset() {
+        level = 1;
+        return this;
+    }
 }
