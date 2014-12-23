d20e5eabaec044aa0eb9fe41b8b65a5c3750f098
ana-balica <ana.balica@gmail.com>
2014-12-23 17:55:48 +0700

    get and display highscores when launching HighscoresActivity

--- app/src/main/java/anabalica/github/io/meowletters/HighscoresActivity.java
+++ app/src/main/java/anabalica/github/io/meowletters/HighscoresActivity.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import anabalica.github.io.meowletters.storage.Highscore;
+import anabalica.github.io.meowletters.storage.HighscoresDataSource;
 
 
 /**
@@ -18,6 +25,9 @@ public class HighscoresActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscores);
+
+        List<Highscore> highscores = getHighscores();
+        displayHighscores(highscores);
     }
 
 
@@ -41,5 +51,40 @@ public class HighscoresActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Retrieve highscores from the highscores table
+     *
+     * @return List of Highscore objects
+     */
+    private List<Highscore> getHighscores() {
+        HighscoresDataSource dataSource = new HighscoresDataSource(this);
+        dataSource.open();
+        List<Highscore> highscores = dataSource.getTopComments();
+        dataSource.close();
+        return highscores;
+    }
+
+    /**
+     * Show the highscores in the layout
+     *
+     * @param highscores List of Highscore objects
+     */
+    private void displayHighscores(List<Highscore> highscores) {
+        RelativeLayout highscoresLayout = (RelativeLayout) findViewById(R.id.highscores);
+        int childrenCount = highscores.size() * 2;
+        int highscoreCounter = 0;
+
+        for (int i = 0; i < childrenCount; i += 2) {
+            TextView playerTextView = (TextView) highscoresLayout.getChildAt(i);
+            TextView scoreTextView = (TextView) highscoresLayout.getChildAt(i + 1);
+            Highscore highscore = highscores.get(highscoreCounter);
+
+            String username = String.valueOf(highscoreCounter + 1) + ". " + highscore.getUsername();
+            playerTextView.setText(username);
+            scoreTextView.setText(String.valueOf(highscore.getScore()));
+            highscoreCounter++;
+        }
     }
 }
