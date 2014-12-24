72360254c9036fb0c052d383c2838b89da2f7a02
ana-balica <ana.balica@gmail.com>
2014-12-24 20:42:12 +0700

    remove onCreateOptionsMenu and onOptionsItemSelected from the activities

--- app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
+++ app/src/main/java/anabalica/github/io/meowletters/GameActivity.java
@@ -73,28 +73,6 @@ public class GameActivity extends Activity {
         drawLetterButtons();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * Select a letter from the grid and add it to the letter chain.
      */
