43b8b07e87ebc98114f7ee31801a9b93ef5b022c
ana-balica <ana.balica@gmail.com>
2014-12-26 18:20:46 +0700

    rename AboutActivity to CreditsActivity

---
+++ app/src/main/java/anabalica/github/io/meowletters/CreditsActivity.java
@@ -0,0 +1,19 @@
+package anabalica.github.io.meowletters;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * This is the activity that contains information about the game, the authors,
+ * the open source project behind it.
+ *
+ * @author Ana Balica
+ */
+public class CreditsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
