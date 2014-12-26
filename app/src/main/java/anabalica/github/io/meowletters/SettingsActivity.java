e27e79f9471cb7a4a633251e88ec660bb6bfe129
ana-balica <ana.balica@gmail.com>
2014-12-26 15:42:03 +0700

    remove OnCreateOptionsMenu and onOptionsItemSelected from SettingsActivity

--- app/src/main/java/anabalica/github/io/meowletters/SettingsActivity.java
+++ app/src/main/java/anabalica/github/io/meowletters/SettingsActivity.java
@@ -2,8 +2,6 @@ package anabalica.github.io.meowletters;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 /**
@@ -27,27 +25,5 @@ public class SettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
