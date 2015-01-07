dcf66821861e476214533c54d8ea1135f9ab9284
ana-balica <ana.balica@gmail.com>
2015-01-08 00:03:48 +0700

    partial class for saving/loading game state

--- app/src/main/java/anabalica/github/io/meowletters/utils/State.java
+++ app/src/main/java/anabalica/github/io/meowletters/utils/State.java
@@ -1,13 +1,34 @@
 package anabalica.github.io.meowletters.utils;
 
+import android.content.SharedPreferences;
+
+import anabalica.github.io.meowletters.GameActivity;
+import anabalica.github.io.meowletters.metrics.Level;
+import anabalica.github.io.meowletters.metrics.Score;
+
 /**
- * Enumeration to keep all game states
+ * Helper class to store and load GameActivity instance for persistence.
+ * Kind of serialization/deserialization in shared preferences.
  *
  * @author Ana Balica
  */
-public enum State {
-    PAUSE,
-    RESUME,
-    RUN,
-    STOP
+public class State {
+    private SharedPreferences sharedPreferences;
+
+    public static final String STATE_LEVEL = "anabalica.github.io.meowletters.state_level";
+    public static final String STATE_SCORE = "anabalica.github.io.meowletters.state_score";
+
+    public State(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    public void save(GameActivity gameActivity) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        // save metrics
+        Level level = gameActivity.getLevel();
+        Score score = gameActivity.getScore();
+        editor.putInt(STATE_LEVEL, level.getLevel());
+        editor.putInt(STATE_SCORE, score.getPoints());
+        editor.apply();
+    }
 }
