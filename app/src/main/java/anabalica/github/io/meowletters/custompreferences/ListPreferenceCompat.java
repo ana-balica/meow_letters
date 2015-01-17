d7116a3eaa36a9dd0f5b1709d4a55203463bc1ae
ana-balica <ana.balica@gmail.com>
2015-01-17 18:27:53 +0700

    fix bug in API level 16 with ListPreference

---
+++ app/src/main/java/anabalica/github/io/meowletters/custompreferences/ListPreferenceCompat.java
@@ -0,0 +1,40 @@
+package anabalica.github.io.meowletters.custompreferences;
+
+import android.content.Context;
+import android.os.Build;
+import android.preference.ListPreference;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+// Fix
+// http://stackoverflow.com/a/21642401/4404583
+public class ListPreferenceCompat extends ListPreference {
+
+    public ListPreferenceCompat(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ListPreferenceCompat(Context context) {
+        super(context);
+    }
+
+    // NOTE:
+    // The framework forgot to call notifyChanged() in setValue() on previous versions of android.
+    // This bug has been fixed in android-4.4_r0.7.
+    // Commit: platform/frameworks/base/+/94c02a1a1a6d7e6900e5a459e9cc699b9510e5a2
+    // Time: Tue Jul 23 14:43:37 2013 -0700
+    //
+    // However on previous versions, we have to workaround it by ourselves.
+    @Override
+    public void setValue(String value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            super.setValue(value);
+        } else {
+            String oldValue = getValue();
+            super.setValue(value);
+            if (!TextUtils.equals(value, oldValue)) {
+                notifyChanged();
+            }
+        }
+    }
+}
